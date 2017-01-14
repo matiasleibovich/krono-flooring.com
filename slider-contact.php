a456513b30f0c1855164cd8205a9ffa1e7aac1a1
matiasleibovich <matias.leibovich@gmail.com>
2017-01-14 08:36:23 -0300

    contact info

--- slider-contact.php
+++ slider-contact.php
@@ -1,13 +1,15 @@
                     <a name="contact-slider-na">
                     <div class="sidebar-box">
 	                    <div class="call-now"><span><?=$dict[$_SESSION['language']]['contact_us_title']?></span></div>
-	                    <div class="call-now" style="background:none;"><span style="background-image: url(../images/flags/argentina.png);">(11) 5517-5220</span></div>
-	                    <div class="call-now" style="background:none; padding-top:5px; padding-right: 10px; text-align:right;">
+	                    <div class="call-now" style="background:none;"><span style="background-image: url(../images/flags/argentina.png); margin-left:4px;">(11) 5517-5220</span></div>
+	                    <div class="call-now" style="background:none;"><a href="mailto:marisa@sostenmutuo.com.ar"><span style="background-image: url(../images/icons/email.png);"><font style="font-size:18px;">marisa@sostenmutuo.com.ar</font></span></a></div>
+
+	                    <!--<div class="call-now" style="background:none; padding-top:5px; padding-right: 10px; text-align:right;">
 	                    	<img src="images/icons/skype.png" style="margin: 3px; margin-left: 10px;">
 	                    	<img src="images/icons/whatsapp.png" style="margin: 3px;">
 	                    	<img src="images/icons/hangouts.png" style="margin: 3px;">
 	                    	<img src="images/icons/linked.png" style="margin: 3px;">
 	                    	<img src="images/icons/fbk.png" style="margin: 5px;">
 	                    	<img src="images/icons/twit.png" style="margin: 5px;">
-	                    </div>
+	                    </div>-->
                     </div>
